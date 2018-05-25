38443fe80bff65e39d53299764aa675d043eee51
Adam L <adamjl@bgsu.edu>
2018-05-24 20:47:52 -0400

    added initial navigation test

--- ProjectTwo/src/test/java/com/revature/tests/CaliberSiteNavigationTest.java
+++ ProjectTwo/src/test/java/com/revature/tests/CaliberSiteNavigationTest.java
@@ -4,9 +4,7 @@ import static org.testng.Assert.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -14,7 +12,7 @@ import org.testng.annotations.Test;
 
 import com.revature.driver.DriverFactory;
 
-public class GoogleTest {
+public class CaliberSiteNavigationTest {
 
 	WebDriver driver;
 	WebDriverWait wait;
@@ -32,35 +30,16 @@ public class GoogleTest {
 	}
 	
 	@Test(priority=1)
-	public void navigateToGoogle() {
-		driver.get("http://google.com");
+	public void navigateToCaliber() {
+		driver.get("https://dev-caliber.revature.tech/");
 	}
 
 	@Test(priority=2)
-	public void validateSuccessfulNavigationToGoogle() {
-		assertEquals("Google", driver.getTitle());
+	public void validateSuccessfulNavigationToCaliber() {
+		assertEquals("Caliber | Performance Management", driver.getTitle());
 	}
 	
-	@Test(priority=3)
-	public void automationTesting() {
-		driver.findElement(By.id("lst-ib")).sendKeys("cats");
-	}
 	
-	@Test(priority=4)
-	public void submitAutomationTestingSearch() {
-//		driver.findElement(By.name("btnK")).submit();
-		wait.until(ExpectedConditions.elementToBeClickable(By.name("btnK"))).submit();
-	}
-	
-	@Test(priority=5)
-	public void validateSuccessfulSearch() {
-		assertEquals("cats - Google Search", driver.getTitle());
-	}
-	
-	@Test(priority=6)
-	public void clickImages() {
-		driver.findElement(By.linkText("Images")).click();
-	}
 	
 	
 	
