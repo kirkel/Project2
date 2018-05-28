0352ead65d0ae0177ae34b66df8a08a8d8b1377c
Kirk Legarda <kirk.legarda@gmail.com>
2018-05-28 12:02:20 -0400

    small change in in pom for intelliJ

--- ProjectTwo/src/main/java/com/revature/runners/TestNGRunner.java
+++ ProjectTwo/src/main/java/com/revature/runners/TestNGRunner.java
@@ -4,8 +4,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-		features = {"src/test/CaliberHomePage.feature", "src/test/CaliberAssessBatch.feature", 
-				"src/test/CaliberManageBatch.feature", "src/test/CaliberReports.feature", },
+//		features = {"src/test/CaliberHomePage.feature", "src/test/CaliberAssessBatch.feature",
+//				"src/test/CaliberManageBatch.feature", "src/test/CaliberReports.feature", },
+		features = {"src/test/CaliberHomePage.feature"},
 		glue= {"com.revature.stepimpl"}
 	)
 public class TestNGRunner extends AbstractTestNGCucumberTests{
