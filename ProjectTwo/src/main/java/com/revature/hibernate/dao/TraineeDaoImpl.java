a33855bede58b53dd0eb50a472e79df0eb7b0491
David <mrprado@gmail.com>
2018-05-29 20:09:41 -0400

    finished dao imples

--- ProjectTwo/src/main/java/com/revature/hibernate/dao/TraineeDaoImpl.java
+++ ProjectTwo/src/main/java/com/revature/hibernate/dao/TraineeDaoImpl.java
@@ -2,7 +2,13 @@ package com.revature.hibernate.dao;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.hibernate.entity.Assessment;
 import com.revature.hibernate.entity.Trainee;
+import com.revature.hibernate.util.HibernateUtil;
 
 public class TraineeDaoImpl implements TraineeDao{
 
@@ -18,25 +24,95 @@ public class TraineeDaoImpl implements TraineeDao{
 	
 	@Override
 	public List<Trainee> getAllTrainees() {
-		// TODO Auto-generated method stub
+		Session session = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			return session.createQuery("from Trainee", Trainee.class).getResultList();
+		} catch (HibernateException hbe) {
+			hbe.printStackTrace();
+		} finally {
+			HibernateUtil.shutdownSession(session);
+		}
 		return null;
 	}
 
 	@Override
 	public boolean insertTrainee(Trainee trainee) {
-		// TODO Auto-generated method stub
+		Session session = null;
+		Transaction t = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			t = session.beginTransaction();
+			session.save(trainee);
+			t.commit();
+			System.out.println(trainee.toString() + " successfully inserted");
+			return true;
+		} catch (HibernateException hbe) {
+			if (t != null) {
+				t.rollback();
+				System.out.println("Transaction successfully rolled back");
+			}
+			hbe.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+				System.out.println("Session successfully closed: " + !session.isOpen());
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public boolean updateTrainee(Trainee trainee) {
-		// TODO Auto-generated method stub
+		Session session = null;
+		Transaction t = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			t = session.beginTransaction();
+			Trainee temp = session.get(Trainee.class, trainee.getTrainee_Id());
+			//temp.setName(pokemon.getName());
+			session.update(temp);
+			t.commit();
+			System.out.println(trainee.toString() + " successfully updated");
+			return true;
+		} catch (HibernateException hbe) {
+			if (t != null) {
+				t.rollback();
+				System.out.println("Transaction successfully rolled back");
+			}
+			hbe.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+				System.out.println("Session successfully closed: " + !session.isOpen());
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public boolean deleteTrainee(Trainee trainee) {
-		// TODO Auto-generated method stub
+		Session session = null;
+		Transaction t = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			t = session.beginTransaction();
+			session.delete(trainee);
+			t.commit();
+			System.out.println(trainee.toString() + " successfully deleted");
+			return true;
+		} catch (HibernateException hbe) {
+			if (t != null) {
+				t.rollback();
+				System.out.println("Transaction successfully rolled back");
+			}
+			hbe.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+				System.out.println("Session successfully closed: " + !session.isOpen());
+			}
+		}
 		return false;
 	}
 
--- ProjectTwo/src/main/java/com/revature/hibernate/dao/WeekDaoImpl.java
+++ ProjectTwo/src/main/java/com/revature/hibernate/dao/WeekDaoImpl.java
@@ -2,7 +2,13 @@ package com.revature.hibernate.dao;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.hibernate.entity.Assessment;
 import com.revature.hibernate.entity.Week;
+import com.revature.hibernate.util.HibernateUtil;
 
 public class WeekDaoImpl implements WeekDao{
 
@@ -18,26 +24,97 @@ public class WeekDaoImpl implements WeekDao{
 	
 	@Override
 	public List<Week> getAllWeeks() {
-		// TODO Auto-generated method stub
+		Session session = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			return session.createQuery("from Week", Week.class).getResultList();
+		} catch (HibernateException hbe) {
+			hbe.printStackTrace();
+		} finally {
+			HibernateUtil.shutdownSession(session);
+		}
 		return null;
 	}
 
 	@Override
 	public boolean insertWeek(Week week) {
-		// TODO Auto-generated method stub
+		Session session = null;
+		Transaction t = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			t = session.beginTransaction();
+			session.save(week);
+			t.commit();
+			System.out.println(week.toString() + " successfully inserted");
+			return true;
+		} catch (HibernateException hbe) {
+			if (t != null) {
+				t.rollback();
+				System.out.println("Transaction successfully rolled back");
+			}
+			hbe.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+				System.out.println("Session successfully closed: " + !session.isOpen());
+			}
+		}
 		return false;
 	}
+
 
 	@Override
 	public boolean updateWeek(Week week) {
-		// TODO Auto-generated method stub
+		Session session = null;
+		Transaction t = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			t = session.beginTransaction();
+			Week temp = session.get(Week.class, week.getWeek_Id());
+			//temp.setName(pokemon.getName());
+			session.update(temp);
+			t.commit();
+			System.out.println(week.toString() + " successfully updated");
+			return true;
+		} catch (HibernateException hbe) {
+			if (t != null) {
+				t.rollback();
+				System.out.println("Transaction successfully rolled back");
+			}
+			hbe.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+				System.out.println("Session successfully closed: " + !session.isOpen());
+			}
+		}
 		return false;
 	}
-
 	@Override
 	public boolean deleteWeek(Week week) {
-		// TODO Auto-generated method stub
+		Session session = null;
+		Transaction t = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			t = session.beginTransaction();
+			session.delete(week);
+			t.commit();
+			System.out.println(week.toString() + " successfully deleted");
+			return true;
+		} catch (HibernateException hbe) {
+			if (t != null) {
+				t.rollback();
+				System.out.println("Transaction successfully rolled back");
+			}
+			hbe.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+				System.out.println("Session successfully closed: " + !session.isOpen());
+			}
+		}
 		return false;
 	}
 
 }
+
