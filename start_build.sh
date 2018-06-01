fa1432c8497b4478a12b2055b72079962b35be04
Kirk Legarda <kirk.legarda@gmail.com>
2018-06-01 11:54:24 -0400

    modified start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -1,3 +1,9 @@
+pwd
+ls -alt
 cd testAngular/testAngular/
-npm install
-ng build --prod --base-href=/testAngular/testAngular/
+npm install	
+ng build --prod --base-href=http://ec2-54-243-8-17.compute-1.amazonaws.com:8090/projecttwoweb/
+cd dist/
+pwd
+ls -alt
+\cp -R testAngular/ /c/Program\ Files/Apache\ Software\ Foundation/Tomcat\ 8.5/webapps/
