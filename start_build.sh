1759a5d63b5a820112ba2c34a0a22b9760d870da
Kirk Legarda <kirk.legarda@gmail.com>
2018-06-02 22:37:30 -0400

    script for ProjectTwo/.../projecttwoweb

--- start_build.sh
+++ start_build.sh
@@ -1,12 +1,16 @@
 pwd
 ls -alt
-cd testAngular/testAngular/
+cd ProjectTwo/src/main/webapp/projecttwoweb/
 pwd
 ls -alt
-npm install	
-ng build --prod --base-href=http://localhost:8090/testAngular/
-pwdls -alt
-cd dist/
+npm install
 pwd
 ls -alt
-\cp -R testAngular/ /c/Program\ Files/Apache\ Software\ Foundation/Tomcat\ 8.5/webapps/
+ng build --prod --base-href=http://ec2-54-89-63-71.compute-1.amazonaws.com:8090/projecttwoweb/
+pwd
+ls -alt
+cd  dist/
+\cp -R projecttwoweb/ /c/Program\ Files/Apache\ Software\ Foundation/Tomcat\ 8.5/webapps/
+cd /c/Program\ Files/Apache\ Software\ Foundation/Tomcat\ 8.5/webapps/
+pwd
+ls -alt
